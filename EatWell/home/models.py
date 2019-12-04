26392df0438208b85243d1613cf92d9e012d5721
twishaavyas <twishaavyas@gmail.com>
2019-12-04 15:57:18 -0500

    added new models

--- EatWell/home/models.py
+++ EatWell/home/models.py
@@ -11,6 +11,7 @@ class EmergencyContact(models.Model):
 class Ingredient(models.Model):
 	id = models.AutoField(primary_key=True)
 	ingredient_name  = models.CharField(max_length=100)
+	is_main_ingredient = models.BooleanField(default = False)
 
 class User(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -18,11 +19,26 @@ class User(models.Model):
 	last_name = models.CharField(max_length=100)
 	username = models.CharField(max_length=100, unique=True, default = "not_applicable")
 	contact = models.CharField(max_length=10)
-	user_name = models.CharField(max_length=100, default="")
 	emergency_contact = models.OneToOneField(EmergencyContact, on_delete = models.CASCADE)
 
 class UserAllergy(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     allergy = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+class Category(models.Model):
+	id = models.AutoField(primary_key=True)
+	category_name  = models.CharField(max_length=100)
+
+class Product(models.Model):
+	id = models.AutoField(primary_key=True)
+	product_name  = models.CharField(max_length=100)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class ProductIngredients(models.Model):
+	product = models.ForeignKey(User, on_delete=models.CASCADE)
+	ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+
+
 
 
